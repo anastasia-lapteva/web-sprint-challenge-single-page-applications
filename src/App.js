bf4f8745dc7dcbf3a5905aa4c3b088c37e0ce45a
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-09-17 12:16:29 -0700

    complete without cypress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import Home from "./Home";
 import Order from "./Order";
 import Confirmation from "./Confirmation";
